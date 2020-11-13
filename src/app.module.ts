0b1236b87ffe4d2266a3408be0ae6cc1c48ad265
luiz barbosa <luizbp222@hotmail.com>
2020-11-13 17:20:22 -0300

    addMongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/customer-app', { useNewUrlParser: true })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
